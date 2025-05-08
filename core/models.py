a997cbf77ab06deb979066cb483c32c3d3571a05
EvanShapedDesktop <2020nave@gmail.com>
2025-05-08 16:13:37 -0400

    Add initial models to django ORM for my Projects, the Jobs I worked them at, and the Technologies used for each Project. Unfinished but working

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    company_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    start_date = models.DateField(null=False)
+    end_date = models.DateField(null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.job_title} at {self.company_name}"
+
+class Project(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    difficulty_rating = models.IntegerField(default=1)
+    start_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Technology(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.URLField(max_length=300)
+
+    def __str__(self):
+        return self.name
