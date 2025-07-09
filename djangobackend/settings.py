bf71d886bb44f330a27522f32a715ab78d4b1e20
EvanShapedDesktop <2020nave@gmail.com>
2025-07-08 21:00:41 -0400

    Changed django logging configuration. Started from default config. Included levelname in all logs. Changed level to debug for both django and django.server loggers (default was INFO)

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -28,6 +28,60 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = True
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "django.server": {
+            "()": "django.utils.log.ServerFormatter",
+            "format": "[{levelname}] [{server_time}] {message}",
+            "style": "{",
+        },
+        "showlevel": {
+            "format": "[{levelname}] {message}",
+            "style": "{",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+            "formatter": "showlevel",
+        },
+        "django.server": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "django.server",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+            "formatter": "showlevel",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "mail_admins"],
+            "level": "DEBUG",   # default was "info"
+        },
+        "django.server": {
+            "handlers": ["django.server"],
+            "level": "DEBUG",   # default was "info"
+            "propagate": False,
+        },
+    },
+}
+
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
