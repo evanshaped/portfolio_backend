acf3d8772e968dec6638069c1e1c86ea8fb0a6da
EvanShapedDesktop <2020nave@gmail.com>
2025-07-10 21:36:56 -0400

    Make idiom.regex field blank=True. Also simplify idiom to_string

--- idioms/models.py
+++ idioms/models.py
@@ -13,10 +13,10 @@ def get_default_language():
 class Idiom(models.Model):
     text = models.TextField()
     language = models.ForeignKey(Language, on_delete=models.SET_DEFAULT, default=get_default_language)
-    regex = models.TextField(null=True)
+    regex = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return f"Idiom is {self.text} with regex {self.regex}" if self.regex else f"Idiom is {self.text}"
+        return f"{self.text} (regex {self.regex})" if self.regex else f"{self.text}"
 
 class Corpus(models.Model):
     name = models.CharField(max_length=100)
