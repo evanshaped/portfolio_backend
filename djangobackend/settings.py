d9b34c3c97125144843f491ef4dec65cb87081e1
EvanShapedDesktop <2020nave@gmail.com>
2025-07-22 01:18:46 -0400

    Make rest framework default to read only permissions for unauthenticated users

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -16,6 +16,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Django Rest Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
